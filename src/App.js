0a88f1f77f02e6c5851993f1c412038c397c2402
Arun <arunkumar.venkatesan@arcadix.com>
2022-12-20 10:57:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>     
-             Arcadix Cloudflare React Demo
+             Arcadix Cloudflare React Demo -- Preview check
         </p>
         
       </header>
