93a67aadee8c631daccba040b7d1381bf6efb783
Arun <arunkumar.venkatesan@arcadix.com>
2022-12-22 14:22:43 +0530

    update

--- functions/apis/do/do_sample.js
+++ functions/apis/do/do_sample.js
@@ -1,6 +1,6 @@
 export async function onRequestGet(context) {  
-    const id = context.env.DO_Sample.newUniqueId();
-    const stub = context.env.DO_Sample.get(id);
+    const id = context.env.DO_Sample2.newUniqueId();
+    const stub = context.env.DO_Sample2.get(id);
   
     // Pass the request down to the durable object
     return stub.fetch(context.request);
